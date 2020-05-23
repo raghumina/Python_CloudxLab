d1b4ceae3a0a9bf0e4b7996ee04cc728762e3b5d
raghu <raghumina11@gmail.com>
2020-05-22 20:53:09 -0700

    if and Elif

---
+++ Hello.py
@@ -0,0 +1,22 @@
+# Started python basics from CloudxLabs
+# course name = Python for Beginners
+
+# A random program using if and elif
+
+print("Where are you from ,# please write in upper cases ")
+print("""the available countries in our list are 
+India,
+England,
+France,
+Germany""")
+country = input("Please enter your Input\n")
+if country == "India":
+    print("Namste")
+elif country == "England":
+    print("hello")
+elif country == "France":
+    print("Bonjour")
+elif country == "Germany":
+    print("Guten Tag")
+else:
+    print("hello hhola ")
