14399347e89fcdc9255a690c81dc285b5a139f4b
raghu <raghumina11@gmail.com>
2020-05-26 06:18:01 -0700

    Larger and smaller numbers

--- StringLoop.py
+++ StringLoop.py
@@ -34,11 +34,25 @@ print("the sum is :", sum)
 print("the average is :", sum / count)
 '''
 
-x = [1,2,45,6,7,54,]
+x = [1, 2, 45, 6, 7, 54, ]
 print(max(x))  # to find the maximum value in a list
 
-min_numbers = [21,34,565,77,12,454,4454]
-print(min(min_numbers)) # to find the minimum number in a list
+min_numbers = [21, 34, 565, 77, 12, 454, 4454]
+print(min(min_numbers))  # to find the minimum number in a list
 
-list = ["banana",22,34,454,"tomato"]
-print(len(list))  # to find the length of a list 
+list = ["banana", 22, 34, 454, "tomato"]
+print(len(list))  # to find the length of a list
+
+# Applying some filters in a loop
+# for example to print the largest numbers or smallest nnumbers
+# by applying conditions
+
+for numbers in [1, 2, 3, 4, 5, 6, 7, 8, 9, 7, 6, 5, 4, 4, 3, 2, 2]:
+    print(numbers)
+    if numbers > 5:
+        print("the larger numbers are")
+
+
+print("get the answer")
+
+# same method we can apply for the smaller numbers 
