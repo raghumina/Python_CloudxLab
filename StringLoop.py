76152d7a98b5ef46a8c3a305a9eded9c8e557336
raghu <raghumina11@gmail.com>
2020-05-26 05:44:12 -0700

    Count, Sum, Average of list using loops

---
+++ StringLoop.py
@@ -0,0 +1,36 @@
+# counting the list
+# by using the loop
+'''
+count = 0
+print("count")
+for numbers in [2, 5, 6, 4, 3, 2, 4, 6]:
+    print("numbers")
+    count = count + 1
+    print(count, numbers)
+print("after ", count)
+
+# submission of the list
+# using loops
+'''
+'''
+sum = 0
+for numbers in [3, 5, 6, 7, 85, 32, 5, 4, 2]:
+    sum = sum + numbers
+    print(sum, numbers)
+print("got it ")
+'''
+
+# Finding the average in a loop
+count = 0  # to count the numbers
+sum = 0  # for the sum of the numbers
+print(count, sum)
+for numbers in [2, 4, 5, 7, 8, 6, 5, 4, 3, 223]:
+    count = count + 1
+    sum = sum + numbers
+    print(count, sum)
+# print(count,sum)
+print("The count is :", count)
+print("the sum is :", sum)
+print("the average is :", sum / count)
+
+
