474e0237d51151c04a2ef52330962785300de407
raghu <raghumina11@gmail.com>
2020-05-26 06:03:59 -0700

    Min, Max, len list functions

--- StringLoop.py
+++ StringLoop.py
@@ -19,7 +19,7 @@ for numbers in [3, 5, 6, 7, 85, 32, 5, 4, 2]:
     print(sum, numbers)
 print("got it ")
 '''
-
+'''
 # Finding the average in a loop
 count = 0  # to count the numbers
 sum = 0  # for the sum of the numbers
@@ -32,5 +32,13 @@ for numbers in [2, 4, 5, 7, 8, 6, 5, 4, 3, 223]:
 print("The count is :", count)
 print("the sum is :", sum)
 print("the average is :", sum / count)
+'''
 
+x = [1,2,45,6,7,54,]
+print(max(x))  # to find the maximum value in a list
 
+min_numbers = [21,34,565,77,12,454,4454]
+print(min(min_numbers)) # to find the minimum number in a list
+
+list = ["banana",22,34,454,"tomato"]
+print(len(list))  # to find the length of a list 
