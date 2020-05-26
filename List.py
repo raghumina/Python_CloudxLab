32e3a65356eb99edb93f2f0bb99b180555d7c9ca
raghu <raghumina11@gmail.com>
2020-05-26 05:22:31 -0700

    List

---
+++ List.py
@@ -0,0 +1,39 @@
+'''
+# List in python are like arrays
+# they stores values of different types
+# such as string , int , float etc
+players = ["Messi", "Ronaldo", "Neymar", "Suarez"]  # this is a simple list
+players.append("Inesta")  # the append is use to add a value in the varaible
+print(players)  # append adds a value in the end
+
+for players in ["Messi", "Ronaldo", "Neymar", "Suarez"]:  # this will check and print all the values in the list
+    print(players)
+print("They are the best attackers in football\n"
+      "best of the best")
+
+dir(players)
+
+# one more example
+# to find the largest number in a list
+
+# largest_number = -1
+# print(largest_number)
+# for the_num in [11, 43, 56, 665, 23]:
+#    if the_num > largest_number:
+#        largest_number = the_num
+#        print(largest_number, the_num)
+
+# print("gottcha",largest_number)
+
+
+# max([11, 43, 56, 665, 23])
+print("the largest number is", max([11, 43, 56, 665, 23]))  # we can also use max function
+# to find the largest value in list
+'''
+the_stock = -1
+for stock in [1,2,3,2,4,7,5,4,6,8,9,6]:
+    print(stock)
+    if stock > the_stock:
+        the_stock = stock
+        print(the_stock,stock)
+print("you should sell",the_stock,"for maximum profit")
--- Loops.py
+++ Loops.py
@@ -24,16 +24,26 @@
 # sample program
 # with continue
 
+#while True:
+#    line = input(" >")
+#    if line == '#':  # this will continue the loop
+#        continue  # until the next condition is met
+#   if line == 'done':
+#       break
+#       print(line)
+
+#print('completed')
+import turtle
 while True:
-    line = input(" >")
-    if line == '#':  # this will continue the loop
-        continue     # until the next condition is met
-    if line == 'done':
+    line = input("Please Enter your name\n ")
+    line2 = int(input("Please enter your age\n"))
+    line3 = input("Please enter your city ")
+    line4 = int(input("Please enter your zip code"))
+    if line4 == 302033:
         break
-        print(line)
-
-print('completed')
-
-
-
-
+        print("You are from Rajasthan ")
+        for steps in range(50):
+            turtle.color("green")
+            turtle.forward(100)
+            turtle.right(90)
+print("complete")
