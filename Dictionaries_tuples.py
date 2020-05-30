d9905fb8ecb9301568e2265ba3c139299065d4db
raghu <raghumina11@gmail.com>
2020-05-30 10:12:57 -0700

    Update Dictionaries_tuples.py

--- Dictionaries_tuples.py
+++ Dictionaries_tuples.py
@@ -46,3 +46,39 @@ for email in name:
 players = {'Ronaldo ', 'Messi', 'Kaka'}
 realmadrid, Barcelona, Milan = players
 print(Milan, Barcelona, realmadrid)
+
+# date and time module
+
+import datetime as dt
+import time as tm
+
+print(tm.time())
+
+# map function example
+people = ['Dr. Christopher Brooks', 'Dr. Kevyn Collins-Thompson', 'Dr. VG Vinod Vydiswaran', 'Dr. Daniel Romero']
+
+
+def split_title_and_name(person):
+    title = person.split()[0]
+    lastname = person.split()[-1]
+    return '{} {}'.format(title, lastname)  # but it will give a lazy value
+
+
+list(map(split_title_and_name, people))
+
+# lambda function
+
+people = ['Dr. Christopher Brooks', 'Dr. Kevyn Collins-Thompson', 'Dr. VG Vinod Vydiswaran', 'Dr. Daniel Romero']
+
+
+def split_title_and_name(person):
+    return person.split()[0] + ' ' + person.split()[-1]
+
+
+# option 1
+for person in people:
+    print(split_title_and_name(person) == (lambda x: x.split()[0] + ' ' + x.split()[-1])(person))
+
+# option 2
+list(map(split_title_and_name, people)) == list(
+    map(lambda person: person.split()[0] + ' ' + person.split()[-1], people))
