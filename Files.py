b5414cc4bca65cbd20009b9810a091f0c1489e90
raghu <raghumina11@gmail.com>
2020-05-30 10:13:11 -0700

    Update Files.py

--- Files.py
+++ Files.py
@@ -2,5 +2,7 @@
 # open returns a file handle
 
 # lets try
+import csv
 
-file = open("C:/Users/ashok/Desktop/BOOKS/Complete ML in R/1. Linear Regression/Customer.csv")
+file = open("C://Users//ashok//Desktop//BOOKS//Complete ML in R//1. Linear Regression/Customer.csv")
+file = open("Customer.csv", "r")
