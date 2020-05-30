fd8abd723306d98631e67a8bb6247fe277a6a4c9
raghu <raghumina11@gmail.com>
2020-05-29 23:46:24 -0700

    Dictionaries and tuples

---
+++ Dictionaries_tuples.py
@@ -0,0 +1,11 @@
+# Dictionaries and tuples in python
+p = "hello"
+print(len(p))
+
+names = ['harry', 'ron', 'guy', 'lee']
+print("hello", names)
+for names in names:
+    print("happpy new year ", names)
+print(list(names[0]))
+print(len(names))
+print(list(range(len(names))))
