bcfe3df085e69f7102072c2a2a185b53e9ada0ff
raghu <raghumina11@gmail.com>
2020-05-30 10:12:44 -0700

    Numpy library and its various functions

---
+++ NumPy.py
@@ -0,0 +1,31 @@
+# NumPy library
+# A library which is used in python for arrays and matrices
+
+import numpy as np
+
+# creating  a array
+
+my_list = [1,2,4]
+x = np.array(my_list)
+print(x)
+
+y = np.array([1,2,3])
+print(y)
+
+
+z = np.array([[12,2,3],[12,32,12]])
+print(z)
+
+p = np.arange(0,20,4)   # it will start from 0 to 20 with multiplication of 4 until 20 becomes the result
+print(p)
+
+# some more numpy functions are
+
+print(p.reshape)
+
+print(['a', 'b', 'c'] + [1, 2, 3])
+a = ['a', 'b', 'c']
+b =  [1, 2, 3]
+c = a + b
+print(c)
+print( type(lambda x: x+1))
