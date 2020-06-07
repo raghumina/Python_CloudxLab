a902101428cae04268263a60209b4dbf1bf13698
raghu <raghumina11@gmail.com>
2020-06-07 17:37:34 +0530

    Update Conditions.py

--- Conditions.py
+++ Conditions.py
@@ -2,7 +2,7 @@
 # write a program to prompt the user for hours and rate per hour to compute gross pay
 
 # lets start
-hours = int(input("Please enter the hours here\n"))
+hours = float(input("Please enter the hours here\n"))
 rate_per_hour = float(input("Please enter the rate here\n"))
 gross_pay = hours * rate_per_hour
-print(gross_pay)
+print("The gross pay is:",gross_pay)
