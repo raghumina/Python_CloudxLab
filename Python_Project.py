7631b36aea068a744d067119aa5e4b0923140a60
raghu <raghumina11@gmail.com>
2020-06-09 22:12:14 +0530

    Update Python_Project.py

--- Python_Project.py
+++ Python_Project.py
@@ -1,7 +1,14 @@
 # Python projct on Cloudxlab course
 # Python project - Churn Emails
+# i have used the cloudxlab dataset
+# and solved the problem on cloudxlab jupytr account
+
+
+
 # project part 1
 # Count the numbers of lines
+
+
 
 # Define a function number_of_lines
 # Open the file mbox-short.txt which is located at /cxldata/datasets/project/mbox-short.txt
@@ -35,4 +42,20 @@ def count_number_of_lines():
     return count
 
 count_number_of_lines()
+
+# Project part 3
+# Write a function find_email_sent_days which reads the file
+# /cxldata/datasets/project/mbox-short.txt and categorizes each mail message by which day of the week the email was sent.
+#
+# To do this do the following:
+#
+# Open the file and read it line by line
+# Look for lines that start with "From"
+# For those lines which start from "From",
+# then look for the third word and keep a running count of each of the days of the week.
+# How do you find the day of the week, is an exercise for you.
+# Note: You have to store the results in a dictionary. Only store those day of the week that exists.
+# For Example, if there is no line for Mon then it should not be in the dictionary elements.
+#
+# At the end of the program return the contents of your dictionary (order does not matter)
 
