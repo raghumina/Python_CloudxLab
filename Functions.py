f0c24a489704add5c2c9cb5b8d3c9ec45dafa95a
raghu <raghumina11@gmail.com>
2020-06-16 22:21:09 +0530

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -177,18 +177,19 @@ print(x)   # this is not possible with print type
 hours = float(input("Enter hours here: "))
 rate = float(input("Enter rate here: "))
 extra = 0
-def compute_pay(hours,rate):
 
+
+def compute_pay(hours, rate):
     if hours > 40:
         extra = hours - 40
         hours = 40
         pay = hours * rate + 1.5 * rate * extra
-        return  pay
+        return pay
     else:
         return hours * rate
 
 
-print("Pay",compute_pay(hours,rate))
+print("Pay", compute_pay(hours, rate))
 
 '''
 # Program 11
@@ -206,3 +207,58 @@ def stuff():
 
 stuff()
 '''
+
+
+# Program make a simple calculator
+
+# This function adds two numbers
+def add(x, y):
+    return x + y
+
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # Take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # Check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        break
+    else:
+        print("Invalid Input")
+
+print("Thats a simple calculator ")
