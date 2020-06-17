037295989a1b709abd7faf70554d3c058b6a6b40
raghu <raghumina11@gmail.com>
2020-06-17 22:49:55 +0530

    Update Conditions.py

--- Conditions.py
+++ Conditions.py
@@ -182,3 +182,13 @@ try:
     istr = int(astr)
 except:
     istr = -1
+
+
+# Program to show the use of continue statement inside loops
+
+for val in "string":
+    if val == "i":
+        continue
+    print(val)
+
+print("The end")
