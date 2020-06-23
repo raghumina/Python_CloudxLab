0f9823d32dfb60091dd6f58632f9f329f7d71e94
raghu <raghumina11@gmail.com>
2020-06-23 21:23:34 +0530

    Update Loops.py

--- Loops.py
+++ Loops.py
@@ -163,4 +163,8 @@ print(len('banana')*7)
 
 data = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
 pos = data.find('.')
-print(data.find(0,8))
+print(data.find())
+
+text = "X-DSPAM-Confidence:    0.8475";
+text_float = float(text)
+print(text.find([]))
