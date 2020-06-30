1e288a12ae816c9aa1439908a2fc59054a91bfc8
raghu <raghumina11@gmail.com>
2020-06-30 22:01:35 +0530

    Update Files.py

--- Files.py
+++ Files.py
@@ -122,4 +122,13 @@ fh = open("words.txt","r")
 for i in fh:
     ly = i.rstrip()
     print(ly.upper())
-    
+
+# Problem 2
+
+# Write a program that prompts for a file name,
+# then opens that file and reads through the file, looking for lines of the form:
+# X-DSPAM-Confidence:    0.8475
+# Count these lines and extract the floating point values
+# from each of the lines and compute the average of those values and produce an output
+# as shown below. Do not use the sum() function or a variable named sum in your solution.
+
