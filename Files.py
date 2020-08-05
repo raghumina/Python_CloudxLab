879dd3865a819e44f39d07e28b8fbbf616c3735f
raghu <raghumina11@gmail.com>
2020-08-05 22:42:06 +0530

    Update Files.py

--- Files.py
+++ Files.py
@@ -4,20 +4,7 @@
 # lets try
 import csv
 '''
-file = 'Customer.csv'
 
-fields = []
-rows = []
-
-with open('Customer.csv', 'r') as csvFile:
-    csvreader = csv.reader(csvFile)
-
-    fields = next(csvreader)
-
-    for rows in csvreader:
-        rows.append(rows)
-
-        print("Total number of rows %d" % (csvreader.line_num))  # to count the numbers of the rows in the csv file
 
 # printing the field names
 print("the field names are :" + ", ".join(fields for fields in fields))
