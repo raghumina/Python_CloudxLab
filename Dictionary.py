f43a72f48fca8dcde54fa173cc409af87f60fcf0
raghu <raghumina11@gmail.com>
2020-09-08 22:56:15 +0530

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -5,3 +5,24 @@
 # we use {} in dictionaries
 # dictionaries have keys which act as the address or location of the data
 
+# lets create a sample dictionary
+# for example
+
+dict1 = {
+    "Name": "Tom",
+    "Class": 4,
+    "Roll number": 343,
+    "Subject": "PCB"
+}
+
+print(dict1)
+# so a dictionary has been created with the key values
+
+# most of the functions are similar to the other data types
+
+print(dict1["Class"])   # to access the class value
+print(dict1.values())     # it will print all the value of the dictionary (key values )
+print(dict1.keys())    # it will print all key names of the dictionary
+
+print(dict1.popitem())    # it will show the last item of the dictionary
+print(dict1.get("Roll number"))   # to get the roll number or required data 
