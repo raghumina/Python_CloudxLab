2034e5a3a19da53740af641b94e1e583dcc23fe0
raghu <raghumina11@gmail.com>
2020-09-08 21:29:54 +0530

    Create LIST1.py

---
+++ LIST1.py
@@ -0,0 +1,20 @@
+# List in python
+# list in python are the storage of data in python
+# list can be mioxed means we can add different different data in the list like str and int together and so on
+# it is like array in other programming language
+# for example
+
+list1 = [1,23,4,5,6,7]   # it is a lsit of the 5 values stored in data set list list1
+print(list1)
+
+# lets do a another program related to it
+for i in list1:
+    print(i)     # this will show all of the list data in the sequence
+
+# lets apply different different functions on the list
+
+
+# for example .append function
+# this will add the any value or data at the -1 position of the list 
+var = list1.append("tom")
+print(list1)
