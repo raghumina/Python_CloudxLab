303ff943e6e54b85e1a7ea0f42d51dc205ad55ee
raghu <raghumina11@gmail.com>
2020-09-08 21:34:53 +0530

    Update LIST1.py

--- LIST1.py
+++ LIST1.py
@@ -4,17 +4,27 @@
 # it is like array in other programming language
 # for example
 
-list1 = [1,23,4,5,6,7]   # it is a lsit of the 5 values stored in data set list list1
+list1 = [1, 23, 4, 5, 6, 7]  # it is a lsit of the 5 values stored in data set list list1
 print(list1)
 
 # lets do a another program related to it
 for i in list1:
-    print(i)     # this will show all of the list data in the sequence
+    print(i)  # this will show all of the list data in the sequence
 
 # lets apply different different functions on the list
 
 
 # for example .append function
-# this will add the any value or data at the -1 position of the list 
+# this will add the any value or data at the -1 position of the list
 var = list1.append("tom")
 print(list1)
+
+# lets use another function
+# .add function
+
+list1[2] = "tommy"    # this will add the data at the desired position
+print(list1)
+
+
+
+
