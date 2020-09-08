d72bab5e913314400e647db176ae7cf0755d8071
raghu <raghumina11@gmail.com>
2020-09-08 22:42:04 +0530

    Update Tuple.py

--- Tuple.py
+++ Tuple.py
@@ -4,11 +4,11 @@
 # we use ( ) in tuple
 # for example
 
-tuple1 = (1,2,3,4,5,6,7,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8)
+tuple1 = (1, 2, 3, 4, 5, 6, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8)
 print(tuple1)
 
 # most of the functions in tuple are similar to the function in list
 # even most of them are same and can be used on both data colletions
 
 # for example
-print(tuple1.count(8))     # so it will count how many time 8 is repeated in the tuple
+print(tuple1.count(8))  # so it will count how many time 8 is repeated in the tuple
