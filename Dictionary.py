99c08564a704f9b7d50aa27a47f0062415895690
raghu <raghumina11@gmail.com>
2020-09-08 22:41:58 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,7 @@
+# dictionary in python
+# dictionaries are also like other data colletions like tuples list etc
+# dictionaries are also unordered
+# we can use different different data types in a dictionary
+# we use {} in dictionaries
+# dictionaries have keys which act as the address or location of the data
+
