57b9add121b6358e8072a528ba5d89616d1dbbb6
raghu <raghumina11@gmail.com>
2020-09-08 22:35:28 +0530

    Create Tuple.py

---
+++ Tuple.py
@@ -0,0 +1,14 @@
+# tuple in python arte also unordered data collection
+# we can store different different type of data in tuple
+# tuple is not mutable
+# we use ( ) in tuple
+# for example
+
+tuple1 = (1,2,3,4,5,6,7,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8)
+print(tuple1)
+
+# most of the functions in tuple are similar to the function in list
+# even most of them are same and can be used on both data colletions
+
+# for example
+print(tuple1.count(8))     # so it will count how many time 8 is repeated in the tuple
