fc643a7eb39d95013cecb79a98a6b7b594cf9544
raghu <raghumina11@gmail.com>
2020-09-09 22:54:07 +0530

    Update ForLoop.py

--- ForLoop.py
+++ ForLoop.py
@@ -4,7 +4,7 @@
 # for example
 count = 0
 list1 = ["Messi", "Suarez", "Vidal", "Dembele", "Sergio", "Pique"]
-#for palyers in list1:
+# for palyers in list1:
 #    count = count + 1
 #    print(count, palyers)
 
@@ -12,8 +12,8 @@ list1 = ["Messi", "Suarez", "Vidal", "Dembele", "Sergio", "Pique"]
 # what id there is a lsit in a list so how can we access both items or data
 # lets try
 
-season_goals = [["Messi",50], ["Suarez",30], ["Vidal",5], ["Dembele",17], ["Sergio",2], ["Pique",5]]
-#for goals in season_goals:
+season_goals = [["Messi", 50], ["Suarez", 30], ["Vidal", 5], ["Dembele", 17], ["Sergio", 2], ["Pique", 5]]
+# for goals in season_goals:
 #    print(goals)   # this will print it like a list
 
 # the better way to do that is
