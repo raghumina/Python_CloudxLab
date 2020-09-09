e845423137e39f1be5c4f8dc3257796f44a6e103
raghu <raghumina11@gmail.com>
2020-09-09 19:45:03 +0530

    Update Conditionals.py

--- Conditionals.py
+++ Conditionals.py
@@ -52,7 +52,7 @@ if quantity*100 > 1000:
 # the next problem is
 #  company decided to give bonus of 5% to employee if his/her year of service is more than 5 years.
 # Ask user for their salary and year of service and print the net bonus amount.
-
+'''
 year_of_service = int(input("Enter years here : "))
 salary = float(input("Enter salary here : "))
 if year_of_service > 5:
@@ -61,3 +61,27 @@ elif year_of_service == 5:
     print("one more year till bonus")
 else:
     print("no bonus")
+'''
+# next porblem
+# A school has following rules for grading system:
+# a. Below 25 - F
+# b. 25 to 45 - E
+# c. 45 to 50 - D
+# d. 50 to 60 - C
+# e. 60 to 80 - B
+# f. Above 80 - A
+# Ask user to enter marks and print the corresponding grade.
+
+marks = int(input("Enter Marks here : "))
+if marks < 25:
+    print("the grade is F")
+elif marks >= 25 and marks < 45:
+    print("the grade is E")
+elif marks >= 45 and marks < 50:
+    print("the grade is D")
+elif marks >= 50 and marks < 60:
+    print("the grade is C")
+elif marks >=60 and marks < 80:
+    print("the grade is B")
+else:
+    print("the grade is A")
