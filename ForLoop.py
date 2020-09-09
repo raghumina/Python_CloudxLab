a6fe64cdfbfc5f91233a19b52f53a04c79771e3d
raghu <raghumina11@gmail.com>
2020-09-09 23:13:49 +0530

    Update ForLoop.py

--- ForLoop.py
+++ ForLoop.py
@@ -25,7 +25,7 @@ for palyers, goals in season_goals:
 
 # lets apply above program on dictionary
 # data set will be same but converted to dictionary
-
+'''
 season_goals = [["Messi", 50], ["Suarez", 30], ["Vidal", 5], ["Dembele", 17], ["Sergio", 2], ["Pique", 5]]
 
 dict1 = dict(season_goals)
@@ -33,4 +33,18 @@ print(dict1)    # so it will show it as a dictionary as in key and value format
 #
 
 for players , goals in dict1.items():
+    if goals > 40:
+        print(players, goals )
+else:
     print(players,"scored", goals,"goals in 2021")          #
+
+'''
+# PROBLEM
+# CREATE A LIST WHICH CAN CONSISIT OF ANY DATA TYPE BUT WE HAVE TO PRINT ONLY INT DATA TYPE
+# AND THAT SHOULD BE GREATER THAN 6
+
+sample_list = ["Messi",23,1,3,2,3,45,76,"cow","tea",12.21,1.1,6,6.6,6.1,"superman"]
+
+for items in sample_list:
+    if str(items).isnumeric() and items > 6:
+        print(items)
