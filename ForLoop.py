b9ba6ef5ba6b4d6c0cb1ade515d4658a8b4273d2
raghu <raghumina11@gmail.com>
2020-09-09 22:53:59 +0530

    Create ForLoop.py

---
+++ ForLoop.py
@@ -0,0 +1,23 @@
+# for loops in python
+# they are also conditional statements
+
+# for example
+count = 0
+list1 = ["Messi", "Suarez", "Vidal", "Dembele", "Sergio", "Pique"]
+#for palyers in list1:
+#    count = count + 1
+#    print(count, palyers)
+
+
+# what id there is a lsit in a list so how can we access both items or data
+# lets try
+
+season_goals = [["Messi",50], ["Suarez",30], ["Vidal",5], ["Dembele",17], ["Sergio",2], ["Pique",5]]
+#for goals in season_goals:
+#    print(goals)   # this will print it like a list
+
+# the better way to do that is
+
+for palyers, goals in season_goals:
+    count = count + 1
+    print(count, palyers, goals)
