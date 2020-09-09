93ade9b7e51055d89b618a57e34987dd4247c763
raghu <raghumina11@gmail.com>
2020-09-09 19:11:18 +0530

    Update Conditionals.py

--- Conditionals.py
+++ Conditionals.py
@@ -1,6 +1,6 @@
 # if , else , elif are the conditional statements in the python
 # they give the python power to analysis check the condition and give required output
-
+'''
 # so lets start
 # lets check the greater number among the three numbers
 num1 = int(input("Enter number 1:"))
@@ -14,3 +14,26 @@ elif num1 == num2 and num1 == num3 and num2 == num3 and num2 == num1:
     print("all numbers are equal")
 else:
     print("num3 is greatest ")
+'''
+# now lets create a condition for driving if age > 18 allowed to drive and if age < 18 not allowed and
+# if age == 18 also allowed to drive
+'''
+age = int(input("Enter age here : "))
+if age > 18:
+    print("Allowed to drive")
+elif age == 18:
+    print("Allowed to drive")
+else :
+    print("Not allowed to drive ")
+'''
+# lets try another problem
+# Take value of length and breadth and decide weather it is rectangle or not
+
+length = float(input("Enter length here : "))
+breadth = float(input("Enter breadth here : "))
+
+if breadth == length:
+    print("It is a square")
+elif breadth > length or breadth < length:
+    print("this is rectange ")
+
