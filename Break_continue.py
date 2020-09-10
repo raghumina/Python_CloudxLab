ffe352b4042260d354fd2353430478d62b6447b4
raghu <raghumina11@gmail.com>
2020-09-10 21:43:04 +0530

    Update Break_continue.py

--- Break_continue.py
+++ Break_continue.py
@@ -6,29 +6,64 @@ for i in range(0, 100):
   #  if 1 == 1 or i == 2 or i == 99:
         #continue
     print(i)
+
+
+i = 0                    # this becomes a infinite loop
+while i < 45:
+    print(i+1)
+    continue
+    i = i + 1
+
 '''
 
-##
-# problem 1
-# guess a number game
-# lets start
+# lets try to mimic an atm
+# we have total Rupees 10         # we are taking less quantitiy si it will be easy to check the program
 
-num = 26
-print("---------------------------Guess a number game----------------")
-print('''Rules :
-1.You will get 10 chances to guess a number
-2.The pop up will direct you or help you 
-3.If you cant guess in 10 chances the game will be over for you 
-''')
-print("Lets start")
+total = 10
+trans = int(input("Enter amount here: "))
+for i in range(1,11):
+    if i > 5:
+        break
+    print(i)
 
-print("If you want a hint Enter 'Yes'")
-hint = str.upper(input("Enter 'Yes' or 'No': "))
-if hint == "YES":
-    print('''The number is a two digit number 
-    It is less than 41
-    And greater than 10''')
 
-num1 = int(input("Please enter the number you have guessed: "))
+
+print("bye")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
