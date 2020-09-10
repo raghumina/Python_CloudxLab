af2b9e7057e6dced3230a04c10de055eba86f190
raghu <raghumina11@gmail.com>
2020-09-10 17:14:45 +0530

    Create Practice_question_print.py

---
+++ Practice_question_print.py
@@ -0,0 +1,23 @@
+# Practice questions on Print,print,print
+#
+# Question 1
+# Store three integers in x, y and z. Print their sum
+'''
+x = int(input("Enter number here: "))
+y = int(input("Enter number here: "))
+z = int(input("Enter number here: "))
+
+a = x + y + z
+print("The sum of three numbers is: ",a)
+'''
+# Ouestion 2
+#
+# Store two values in x and y and swap them
+
+x = 2
+y = 3
+
+temp = x
+x = y
+y = temp
+print("x:", x, "y:", y)
