b4475e62268979e6884ea9a7eeb5f42adf14a0ef
raghu <raghumina11@gmail.com>
2020-09-10 17:32:29 +0530

    Create While_Loop.py

---
+++ While_Loop.py
@@ -0,0 +1,16 @@
+# while loop
+# the loop which runs until the condition is true
+'''
+# for example
+a = 10
+while a < 20:
+    print("yohooo")    # this loop will run infinitely because condition will never gonna be true
+'''
+
+#
+a = 10
+count = 0
+while a < 20:                        # now it runs until the provided condition gets true 
+    a = a + 1
+    count = count + 1
+    print(count,"yoho")
