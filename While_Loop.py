ee03ad18190608a6e99d62e1c59df5f81941ba1f
raghu <raghumina11@gmail.com>
2020-09-11 17:39:38 +0530

    Update While_Loop.py

--- While_Loop.py
+++ While_Loop.py
@@ -5,7 +5,7 @@
 a = 10
 while a < 20:
     print("yohooo")    # this loop will run infinitely because condition will never gonna be true
-'''
+
 
 #
 a = 10
@@ -17,30 +17,20 @@ while a < 20:  # now it runs until the provided condition gets true
 
 # this loop is similar to the for loop in many ways
 
-
+'''
 ##
 # problem 1
 # guess a number game
 # lets start
 
-num = 26
-print("---------------------------Guess a number game----------------")
-#print('''Rules :
-1.You will get 10 chances to guess a number
-2.The pop up will direct you or help you
-3.If you cant guess in 10 chances the game will be over for you
-#''')
-print("Lets start")
+number = 20
+guess = int(input("Please enter a number: "))
+for guess in range(10, 30):
+    if guess == number:
 
-print("If you want a hint Enter 'Yes'")
-hint = str.upper(input("Enter 'Yes' or 'No': "))
-if hint == "YES":
- #   print('''The number is a two digit number
-    It is less than 41
- #   And greater than 10''')
-
-num1 = int(input("Please enter the number you have guessed: "))
-for num1 in range(9,42):
-
-
-'''
+        print("correct guess")
+        continue
+    elif guess >= 30:
+        print("please guess a smaller number")
+    elif guess <= 10:
+        print("please guess a smaller number ")
