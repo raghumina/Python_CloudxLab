949ff9c6694dfbbb38ef41f7007946f2b64c12c5
raghu <raghumina11@gmail.com>
2020-09-13 21:23:06 +0530

    Create Short_Hand_If_Else.py

---
+++ Short_Hand_If_Else.py
@@ -0,0 +1,25 @@
+# Short Hand If else
+# Short hand conditionals are also known as one liners
+# they are used to write the code in one line
+# make code short
+# but they are not redable proberly so they are not recommended for long and compplex code
+
+# lets try some examples
+# example 1
+a = 3
+b = 4
+# a normal program to print statement
+if a > b:
+    print("A is greater")
+else:
+    print("b is greater ")
+
+# now writing the same program as one liner
+
+print("a is greater")if a > b else print("b is greater ")
+
+
+# and another example is like
+
+num1 = int(input("Enter number: "))
+if num1 > 5: print("Ohk") 
