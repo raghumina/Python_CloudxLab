3cb44ab0748ab051fc5ea88e571b6044916dda50
raghu <raghumina11@gmail.com>
2020-09-13 19:46:41 +0530

    Create Operators.py

---
+++ Operators.py
@@ -0,0 +1,33 @@
+# operators in python
+#
+# Python have many operators
+# most common of them are arithmatic operator
+# like
+#  +
+#  -
+#  *
+#  //
+#  %
+#  **
+
+# there are many more
+# like
+# assignment operator
+# bitwise operator
+# boolean operator
+# identity operator
+# membership operator
+
+
+# example of arithmatic operator
+#
+
+a = 2
+b = 2
+c = a + b    # arithmatic + operator
+print(c)     # more of like mathmatic operators
+print(a * b)   # multiplication operator
+print(a / b)   # divide operator
+print(a // b)   # reminder operator
+print(a ** b)    # power operator
+print(a % b)     # modulo operator
