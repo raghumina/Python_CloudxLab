cb1e7bdf08868fa6035761cca18caf3158e06273
raghu <raghumina11@gmail.com>
2020-09-14 19:08:40 +0530

    Update TryExcept.py

--- TryExcept.py
+++ TryExcept.py
@@ -1,6 +1,8 @@
 # Try Except and Exception in python
 # Exception Handling
-
+# or error handling
+# used where there is a high risk of run time error
+'''
 num1 = input("Enter a number: ")
 num2 = input("Enter number: ")
 try:
@@ -8,3 +10,18 @@ try:
     print(sum)
 except:
     print("Give a numerical value")
+'''
+number1 = input("Enter number: ")
+
+
+try:
+    number1 = int(input("Enter number: "))
+    number2 = int(input("Enter number: "))
+
+
+except Exception:
+    print("Last chance ")
+    print("Pleasew enter int values in input")
+    number1 = int(input("Enter number: "))
+    number2 = int(input("Enter number: "))
+    print("correct input")
