4e2bc4ec3fdb49a1bc3b7cf5b880b3f9dee3fc9b
raghu <raghumina11@gmail.com>
2020-09-14 16:08:10 +0530

    Create TryExcept.py

---
+++ TryExcept.py
@@ -0,0 +1,10 @@
+# Try Except and Exception in python
+# Exception Handling
+
+num1 = input("Enter a number: ")
+num2 = input("Enter number: ")
+try:
+    sum = num1 + num2
+    print(sum)
+except:
+    print("Give a numerical value")
