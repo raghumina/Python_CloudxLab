54ebb9166a39bfaaad27fd13e6dfc686951921b3
raghu <raghumina11@gmail.com>
2020-09-16 19:13:00 +0530

    Update Patterns.py

--- Patterns.py
+++ Patterns.py
@@ -1,8 +1,20 @@
 # create a star pattern with python programming
-k = 1
-for i in range(0, 5):
+# Pattern PRINTING
+# TAKE AN INTGER N
+# BOOLEAN
 
-    for j in range(0, i + 1):
-        print(" ","*", end="")
-
-    print()
+print("How many rows you want")
+row = int(input())
+print("Type 0 or 1")
+bool1 = int(input())
+new = bool(bool1)
+if bool1 == True:
+    for i in range(1,row+1):
+        for j in range(1, i + 1):
+            print("*",end=" ")
+        print()
+elif bool1 == False:
+    for i in range(row,0,-1):
+        for j in range(1, i + 1):
+            print("*",end=" ")
+        print()
