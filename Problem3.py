2e604bb5b4a2d8df1f6308ab8c3b87dcf90a04c5
raghu <raghumina11@gmail.com>
2020-09-19 19:44:39 +0530

    Update Problem3.py

--- Problem3.py
+++ Problem3.py
@@ -1,146 +1,24 @@
-# health management system
-'''
-# Lets start
-def getdate():
-    import datetime
-    return datetime.datetime.now()
+# Health management system
+# we have create a diet and exercise plan for three clients
+# need to create different different files for diet and exercise for each client seperately
+# that means we should be able to retrive or append the file
 
+# first i will try to solve this problem with simple if else
+# then with function
 
-def client_data_append(num1,num2,num3):
-    if num1 == 1 and num2 == 1 and num3 == 2:
-        f1 = open("file1_diet.txt","r+")
-        content1 = f1.write("")
-        print(content1)
-    elif num1 == 1 and num2 == 2 and num3 == 2:
-        f2 = open("file1_exercise.txt","r+")
-        content2 = f2.write("")
-        print(content2)
+# lets try
 
-    elif num1 == 2 and num2 == 1 and num3 == 2:
-        f3= open("file2_diet.txt", "r+")
-        content3 = f3.write("")
-        print(content3)
+client_name = ["TOM", "JERRY", "GOOFY"]
+client_name1 = input("Enter client name: ")
+action = int(input("write the avction you want to take 1 for read and 2 for retrive "))
+action_type = int(input("Enter 1 for diet 2 for excercise: "))
 
-    elif num1 == 2 and num2 == 2 and num3 == 2:
-        f4 = open("file2_exercise.txt", "r+")
-        content4 = f4.write("")
-        print(content4)
+if client_name1 in client_name and client_name1 == "TOM" and action == 1 and action_type == 1:
 
-
-    elif num1 == 3 and num2 == 1 and num3 == 2:
-        f5 = open("file3_diet.txt", "r+")
-        content5 = f5.write("")
-        print(content5)
-
-    elif num1 == 3 and num2 == 2 and num3 == 2:
-        f6 = open("file3_exercise.txt", "r+")
-        content6 = f6.write("")
-        print(content6)
-
-
-
-def retriveData(num1,num2,num3):
-    if num1 == 1 and num2 == 1 and num3 == 1:
-        f1 = open("file1_diet.txt", "r+")
-        content1 = f1.readlines()
-        print(content1)
-    elif num1 == 1 and num2 == 2 and num3 == 1:
-        f2 = open("file1_exercise.txt", "r+")
-        content2 = f2.readlines()
-        print(content2)
-    elif num1 == 2 and num2 == 1 and num3 == 1:
-        f3 = open("file2_diet.txt", "r+")
-        content3 = f3.readlines()
-        print(content3)
-
-    elif num1 == 2 and num2 == 2 and num3 ==1:
-        f4 = open("file2_exercise.txt", "r+")
-        content4 = f4.readlines()
-        print(content4)
-
-    elif num1 == 3 and num2 == 1 and num3 == 1:
-        f5 = open("file3_diet.txt", "r+")
-        content5 = f5.readlines()
-        print(content5)
-
-    elif num1 == 3 and num2 == 2 and num3 == 1:
-        f6 = open("file3_exercise.txt", "r+")
-        content6 = f6.readlines()
-        print(content6)
-
-
-
-print(client_data_append(num1,num2,num3))
-print(retriveData(num1,num2,num3))
-
-KrishnaLive:
-1
-year
-ago
-
-'''
-
-def getdate():
-    import datetime
-    return datetime.datetime.now()
-
-
-def write_dirt(acess_client):
-    if acess_client in list_of_clients:
-        # print(list_of_clients[acess_client])
-        name = acess_client
-        extension = '.txt'
-        f = open(name + extension, 'a')
-        diet_list = input('enter today diet food : ')
-        print(name + 's diet table')
-        heading = 'Diet List and Time '
-        f.write(diet_list)
-        date = str(getdate())
-        f.write(' = ')
-        f.write(date)
-
-        print(getdate())
-        f.write('\n')
-        f.close()
-    else:
-        print('you have write incorrect name .. pleaese check')
-
-
-def write_exercise(acess_client):
-    if acess_client in list_of_clients:
-        # print(list_of_clients[acess_client])
-        name = acess_client
-        extension = '.txt'
-        f = open(name + extension, 'a')
-        exercise_list = input('enter today exercise name : ')
-        print(name + 's Exercise table')
-
-        f.write(exercise_list)
-        date = str(getdate())
-        f.write(' = ')
-        f.write(date)
-
-        print(getdate())
-        f.write('\n')
-        f.close()
-    else:
-        print('you have write incorrect name .. pleaese check')
-
-
-def read_files_of_clients(acess_client):
-    file_name = acess_client
-    extension = '.txt'
-    f = open(file_name + extension, 'rb')
-    print(f.readlines())
-    f.close()
-
-
-list_of_clients = ['harry', 'rohan', 'hamad']
-acess_client = input('enter names to select client and update databese : ')
-do_what = input('what you want to do with client ?  1 for diet 2 for exercise 3 for readfiles')
-if do_what == '1':
-    write_dirt(acess_client)
-if do_what == '2':
-    write_exercise(acess_client)
-if do_what == '3':
-    read_files_of_clients(acess_client)
+    f1 = open(client_name.txt)
+    content1 = f1.read()
+    print(content1)
+elif client_name1 in client_name and client_name1 == "TOM" and action == 1 and action_type == 2:
+    f1 = open(client_name.txt)
+    content1 = f1.read()
+    print(content1)
