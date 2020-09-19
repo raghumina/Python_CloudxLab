1d7084cf1c2ee6bf9689a4b8f15296a7091c8b81
raghu <raghumina11@gmail.com>
2020-09-19 17:34:30 +0530

    Update Problem3.py

--- Problem3.py
+++ Problem3.py
@@ -37,10 +37,36 @@ def client_data(num1,num2):
         content = f6
         print(content)
 
-    elif num1 == 1 and num2 == 2:
-        f = open("file1_exercise.txt", "r+")
-        content = f
-        print(content)
+
+
+def retriveData(num1,num2,num3):
+    if num1 == 1 and num2 == 1 and num3 == 1:
+        f1 = open("file1_diet.txt", "r+")
+        content1 = f1.readlines()
+        print(content1)
+    elif num1 == 1 and num2 == 2 and num3 == 1:
+        f2 = open("file1_exercise.txt", "r+")
+        content2 = f2.readlines()
+        print(content2)
+    elif num1 == 2 and num2 == 1 and num3 == 1:
+        f3 = open("file2_diet.txt", "r+")
+        content3 = f3.readlines()
+        print(content3)
+
+    elif num1 == 2 and num2 == 2 and num3 ==1:
+        f4 = open("file2_exercise.txt", "r+")
+        content4 = f4.readlines()
+        print(content4)
+
+    elif num1 == 3 and num2 == 1 and num3 == 1:
+        f5 = open("file3_diet.txt", "r+")
+        content5 = f5.readlines()
+        print(content5)
+
+    elif num1 == 3 and num2 == 2 and num3 == 1:
+        f6 = open("file3_exercise.txt", "r+")
+        content6 = f6.readlines()
+        print(content6)
 
 
 print(''' THIS IS A HEALTH MANAGMENT SYSTEM
