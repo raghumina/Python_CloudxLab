16979f0aa313d5cb4562f43297a8fe2f07113e37
raghu <raghumina11@gmail.com>
2020-09-19 20:20:02 +0530

    Update Problem3.py

--- Problem3.py
+++ Problem3.py
@@ -15,66 +15,67 @@ action_type = int(input("Enter 1 for diet 2 for excercise: "))
 
 if client_name1 in client_name and client_name1 == "TOM" and action == 1 and action_type == 1:
 
-    f1 = open(client_name.txt)
+    f1 = open("tom_diet.txt")
     content1 = f1.read()
     print(content1)
 
 elif client_name1 in client_name and client_name1 == "TOM" and action == 1 and action_type == 2:
-    f1 = open(client_name.txt)
+    f1 = open("tom_exercise.txt")
     content1 = f1.read()
     print(content1)
 
 elif client_name1 in client_name and client_name1 == "TOM" and action == 2 and action_type == 1:
-    f1 = open(client_name.txt)
-    content1 = f1.read("=")
+    f1 = open("tom_diet.txt","w")
+    content1 = f1.write("")
     print(content1)
 
 elif client_name1 in client_name and client_name1 == "TOM" and action == 2 and action_type == 2:
-    f1 = open(client_name.txt)
-    content1 = f1.read("=")
+    f1 = open("tom_exercise.txt","w")
+ #  diet = input()
+    content1 = f1.write()
     print(content1)
 
 # for jerry
 
 elif client_name1 in client_name and client_name1 == "JERRY" and action == 1 and action_type == 1:
-    f2 = open(client_name.txt)
+    f2 = open("jerry_diet.txt")
     content2 = f2.read()
     print(content2)
 
 elif client_name1 in client_name and client_name1 == "JERRY" and action == 1 and action_type == 2:
-    f2 = open(client_name.txt)
+    f2 = open("jerry_exercise.txt")
     content2 = f2.read()
     print(content2)
 
 elif client_name1 in client_name and client_name1 == "JERRY" and action == 2 and action_type == 1:
-    f2 = open(client_name.txt)
-    content2 = f2.read("=")
+    f2 = open("jerry_diet.txt","w")
+    content2 = f2.read()
     print(content2)
 
 elif client_name1 in client_name and client_name1 == "JERRY" and action == 2 and action_type == 2:
-    f2 = open(client_name.txt)
+    f2 = open("jerry_exercise.txt","w")
     content2 = f2.read("=")
     print(content2)
 
 # for GOOFY
 
 elif client_name1 in client_name and client_name1 == "GOOFY" and action == 1 and action_type == 1:
-    f3 = open(client_name.txt)
+    f3 = open("goofy_diet.txt")
     content3 = f3.read()
     print(content3)
 
 elif client_name1 in client_name and client_name1 == "GOOFY" and action == 1 and action_type == 2:
-    f3 = open(client_name.txt)
+    f3 = open("goofy_exercise.txt")
     content3 = f3.read()
     print(content3)
 
 elif client_name1 in client_name and client_name1 == "GOOFY" and action == 2 and action_type == 1:
-    f3 = open(client_name.txt)
+    f3 = open("goofy_diet.txt","w")
     content3 = f3.read("=")
     print(content3)
 
 elif client_name1 in client_name and client_name1 == "GOOFY" and action == 2 and action_type == 2:
-    f3 = open(client_name.txt)
+    f3 = open("goofy_exercise.txt","w")
     content3 = f3.read("=")
     print(content3)
 
