159eb0c34265b2ef17f03be6f80a7f08be8b358a
raghu <raghumina11@gmail.com>
2020-09-19 19:49:44 +0530

    Update Problem3.py

--- Problem3.py
+++ Problem3.py
@@ -10,7 +10,7 @@
 
 client_name = ["TOM", "JERRY", "GOOFY"]
 client_name1 = input("Enter client name: ")
-action = int(input("write the avction you want to take 1 for read and 2 for retrive "))
+action = int(input("write the avction you want to take 1 for read and 2 for write "))
 action_type = int(input("Enter 1 for diet 2 for excercise: "))
 
 if client_name1 in client_name and client_name1 == "TOM" and action == 1 and action_type == 1:
@@ -18,7 +18,80 @@ if client_name1 in client_name and client_name1 == "TOM" and action == 1 and act
     f1 = open(client_name.txt)
     content1 = f1.read()
     print(content1)
+
 elif client_name1 in client_name and client_name1 == "TOM" and action == 1 and action_type == 2:
     f1 = open(client_name.txt)
     content1 = f1.read()
     print(content1)
+
+elif client_name1 in client_name and client_name1 == "TOM" and action == 2 and action_type == 1:
+    f1 = open(client_name.txt)
+    content1 = f1.read("=")
+    print(content1)
+
+elif client_name1 in client_name and client_name1 == "TOM" and action == 2 and action_type == 2:
+    f1 = open(client_name.txt)
+    content1 = f1.read("=")
+    print(content1)
+
+# for jerry
+
+elif client_name1 in client_name and client_name1 == "JERRY" and action == 1 and action_type == 1:
+    f2 = open(client_name.txt)
+    content2 = f2.read()
+    print(content2)
+
+elif client_name1 in client_name and client_name1 == "JERRY" and action == 1 and action_type == 2:
+    f2 = open(client_name.txt)
+    content2 = f2.read()
+    print(content2)
+
+elif client_name1 in client_name and client_name1 == "TOM" and action == 2 and action_type == 2:
+    f1 = open(client_name.txt)
+    content1 = f1.read("=")
+    print(content1)
+
+elif client_name1 in client_name and client_name1 == "TOM" and action == 2 and action_type == 2:
+    f1 = open(client_name.txt)
+    content1 = f1.read("=")
+    print(content1)
+
+elif client_name1 in client_name and client_name1 == "TOM" and action == 2 and action_type == 2:
+    f1 = open(client_name.txt)
+    content1 = f1.read("=")
+    print(content1)
+
+elif client_name1 in client_name and client_name1 == "TOM" and action == 2 and action_type == 2:
+    f1 = open(client_name.txt)
+    content1 = f1.read("=")
+    print(content1)
+
+elif client_name1 in client_name and client_name1 == "TOM" and action == 2 and action_type == 2:
+    f1 = open(client_name.txt)
+    content1 = f1.read("=")
+    print(content1)
+
+elif client_name1 in client_name and client_name1 == "TOM" and action == 2 and action_type == 2:
+    f1 = open(client_name.txt)
+    content1 = f1.read("=")
+    print(content1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
