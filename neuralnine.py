1146fc02b2e6f880a0b8a3fd4aef2adf15b15576
raghu <raghumina11@gmail.com>
2020-11-12 22:46:31 +0530

    Update neuralnine.py

--- neuralnine.py
+++ neuralnine.py
@@ -25,3 +25,17 @@ canvas = Canvas(width=400, height=300, bg='white')
 canvas.create_line(scale(x1, y1, x2, y2), fill='red')
 canvas.create_line(scale(x2, y2, x3, y3), fill='red')
 canvas.create_line(scale(x3, y3, x1, y1), fill='red')
+
+for i in range(0,100):
+    x4 = ee * x2 + (1 - ee) * x3
+    y4 = ee * y2 + (1 - ee) * y3
+    canvas.create_line(scale(x4,y4,x1,y1),fill = 'red')
+    x3 = x2
+    y3 = y2
+    x2 = x1
+    y2 = y1
+    x1 = x4
+    y1 = y4
+
+canvas.pack()
+mainloop()
